f9b475fec546877a12066508387d9c31e00bf29e
nashi <kausarna@mail.uc.edu>
2024-09-23 21:04:22 -0400

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numberOfKids = 2;
+        boolean isItRaining = false;
+        double priceOfGallonOfGas = 3.89;
+        int favoriteNumber = 7;
+        double shoeSize = 8.5;
+        int birthMonth = 12;
+        String fullName = "Nashita Kausar";
+
+        System.out.println("The number of kids in my family is: " + numberOfKids);
+        System.out.println("It's raining. True or false? " + isItRaining);
+        System.out.println("The price of a gallon of gas is: $" + priceOfGallonOfGas);
+        System.out.println("My favorite number is: " + favoriteNumber);
+        System.out.println("My shoe size is: " + shoeSize);
+        System.out.println("My birth month is: " + birthMonth);
+        System.out.println("My full name is: " + fullName);
+    }
+}
